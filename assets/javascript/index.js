02d3515424ea0c9169b0f18ab5e3c5c9433810d6
mallusrgreatv2 <69511006+mallusrgreatv2@users.noreply.github.com>
2021-02-01 11:24:28 +0530

    seeecret fryday

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,1 +1,8 @@
 console.log(`Hello world`);
+function frick() {
+    const doc = document.getElementById("li1");
+    const img = document.getElementById("img");
+    img.addEventListener("dblclick", () => {
+        doc.innerHTML = "You found the button!\n(it does nothing)"
+    })
+}
